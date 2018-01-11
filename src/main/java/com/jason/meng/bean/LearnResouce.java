4697c83e322eae212ced45b08ac72580e9330dcc
mengdexin <mengdexin@meituan.com>
2018-01-11 21:33:01 +0800

    fix

---
+++ src/main/java/com/jason/meng/bean/LearnResouce.java
@@ -0,0 +1,55 @@
+package com.jason.meng.bean;
+
+/**
+ * @author MengDexin
+ * @date 2018/01/11 20:19
+ **/
+public class LearnResouce {
+
+    private Long id;
+    private String author;
+    private String title;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "LearnResouce{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
